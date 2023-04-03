1e22c346740d8bb8df671051127207ad25ad963e
PREMINDA <premindabandara0777@gmail.com>
2023-04-04 04:38:17 +0530

    setup firebase to application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {UserModule} from './user/user.module';
 import { NavComponent } from './nav/nav.component';
+import {AngularFireModule} from '@angular/fire/compat';
+import {environment} from '../environments/environment';
+import {AngularFireAuthModule} from '@angular/fire/compat/auth';
 
 @NgModule({
   declarations: [
@@ -15,6 +18,8 @@ import { NavComponent } from './nav/nav.component';
     BrowserModule,
     AppRoutingModule,
     UserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
