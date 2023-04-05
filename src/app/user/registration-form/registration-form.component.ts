fba8440411b2767d4faae0a2ca5b4068df2db393
PREMINDA <premindabandara0777@gmail.com>
2023-04-06 05:24:32 +0530

    create user impl

--- src/app/user/registration-form/registration-form.component.ts
+++ src/app/user/registration-form/registration-form.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {AuthService} from '../../services/auth.service';
+import IUser from '../../modals/user.modal';
+
 
 @Component({
   selector: 'app-registration-form',
@@ -7,27 +10,33 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
   styleUrls: ['./registration-form.component.css']
 })
 export class RegistrationFormComponent {
-  name = new FormControl('', [
+  constructor(
+    private auth: AuthService
+  ) {
+
+  };
+
+  name:FormControl = new FormControl('', [
     Validators.required,
     Validators.minLength(3)
   ])
-  email = new FormControl('', [
+  email:FormControl = new FormControl('', [
     Validators.required,
     Validators.email
   ])
-  age = new FormControl('', [
+  age:FormControl = new FormControl('', [
     Validators.required,
     Validators.min(18),
     Validators.max(120)
   ])
-  password = new  FormControl('', [
+  password:FormControl = new  FormControl('', [
     Validators.required,
     Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm)
   ])
-  confirm_password = new FormControl('', [
+  confirm_password:FormControl = new FormControl('', [
     Validators.required
   ])
-  phoneNumber = new FormControl('', [
+  phoneNumber:FormControl = new FormControl('', [
     Validators.required,
     Validators.minLength(13),
     Validators.maxLength(13)
@@ -45,9 +54,26 @@ export class RegistrationFormComponent {
     phoneNumber: this.phoneNumber
   })
 
-  register() {
+  async register() {
     this.showAlert = true
     this.alertMsg = 'Please wait! Your account is being created.'
-    this.alertColor = 'red'
+    this.alertColor = 'blue'
+    if(this.registerForm.valid){
+      const user = this.registerForm.value as unknown as IUser;
+      try {
+        console.log(user)
+        await this.auth.createUser(user)
+      } catch(e) {
+        console.error(e)
+
+        this.alertMsg = 'An unexpected error occurred. Please try again later'
+        this.alertColor = 'red'
+        return
+      }
+
+      this.alertMsg = 'Success! Your account has been created.'
+      this.alertColor = 'green'
+    }
+
   }
 }
