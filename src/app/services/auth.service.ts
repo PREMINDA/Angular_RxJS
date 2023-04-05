3483d700af54aaaa35f858bd0ce8242c34f2a13c
PREMINDA <premindabandara0777@gmail.com>
2023-04-06 05:24:20 +0530

    Auth Service

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import {AngularFireAuth} from '@angular/fire/compat/auth';
+import {AngularFirestore, AngularFirestoreCollection} from '@angular/fire/compat/firestore';
+import IUser from '../modals/user.modal';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private usersCollection: AngularFirestoreCollection<IUser>
+  constructor(
+    private  auth : AngularFireAuth,
+    private db: AngularFirestore
+  ) {
+    this.usersCollection = db.collection('users')
+  }
+
+  public async createUser(userData: IUser) {
+    if(!userData.password) {
+      throw new Error("Password not provided!")
+    }
+
+    const userCred = await this.auth.createUserWithEmailAndPassword(
+      userData.email, userData.password
+    )
+
+    if(!userCred.user) {
+      throw new Error("User can't be found")
+    }
+
+    await this.usersCollection.doc(userCred.user.uid).set({
+      name: userData.name,
+      email: userData.email,
+      age: userData.age,
+      phoneNumber: userData.phoneNumber
+    })
+
+    await userCred.user.updateProfile({
+      displayName: userData.name
+    })
+  }
+
+}
