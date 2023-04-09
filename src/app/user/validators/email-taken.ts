bcc0ee25781d45937873ec43e9e866dff6982729
PREMINDA <premindabandara0777@gmail.com>
2023-04-10 04:31:15 +0530

    async validator for email check

---
+++ src/app/user/validators/email-taken.ts
@@ -0,0 +1,17 @@
+import {AngularFireAuth} from '@angular/fire/compat/auth';
+import {Injectable} from '@angular/core';
+import {AbstractControl, AsyncValidator, ValidationErrors} from '@angular/forms';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmailTaken implements AsyncValidator{
+  constructor(private auth: AngularFireAuth) { }
+
+  validate = (control: AbstractControl) : Promise<ValidationErrors | null> => {
+    return this.auth.fetchSignInMethodsForEmail(control.value).then(
+      response => response.length ? { emailTaken: true } : null
+    )
+  }
+}
