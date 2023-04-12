499d15ee67b91272b904a0c6f4de99db818f1a47
PREMINDA <premindabandara0777@gmail.com>
2023-04-13 05:17:40 +0530

    query param

--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Params, Router} from '@angular/router';
 
 @Component({
   selector: 'app-manage',
   templateUrl: './manage.component.html',
   styleUrls: ['./manage.component.css']
 })
-export class ManageComponent {
+export class ManageComponent implements OnInit{
+  videoOrder = '1';
+  constructor(
+    private router : Router,
+    private route : ActivatedRoute
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.route.queryParamMap.subscribe((params:Params)=>{
+      this.videoOrder = params.sort === '2' ? params.sort : '1';
+
+    })
+
+  }
+
+  async sort(event: Event) {
+    const {value} = (event.target as HTMLSelectElement);
+
+    await this.router.navigate([],{
+      relativeTo:this.route,
+      queryParams:{
+        sort:value
+      }
+    })
+  }
+
 
 }
