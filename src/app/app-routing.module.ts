29371f2e9cb6cc47024537d3f2907e1443e5492d
PREMINDA <premindabandara0777@gmail.com>
2023-04-14 03:35:47 +0530

    Not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import {AboutComponent} from './about/about.component';
 import {ClipComponent} from './clip/clip.component';
+import {NotFoundComponent} from './not-found/not-found.component';
 
 const routes: Routes = [
   {
@@ -18,6 +19,11 @@ const routes: Routes = [
     path: 'clip/:id',
     component: ClipComponent
   },
+  {
+    path:'**',
+    pathMatch: 'full',
+    component:NotFoundComponent
+  }
 ];
 
 @NgModule({
